2a2ea6f9d52259a97287e57fbb084db373dbfa33
JonathanQuang <jquang@stuy.edu>
2017-11-12 14:18:55 -0500

    second Api addition

--- app.py
+++ app.py
@@ -11,6 +11,13 @@ def root():
 	dict=json.loads(data)
 	return render_template("default.html",date=dict["date"],explanation=dict["explanation"],hdurl=dict["hdurl"])
 
+@my_app.route('/second')
+def secondApi():
+    u = urllib2.urlopen('http://api.followthemoney.org/?law-eid=4764641&gro=d-ccb&APIKey=a975fbe4aa68e017785f48ba78bea504&mode=json')
+    data=u.read()
+    dict=json.loads(data)
+    return render_template('secondTemplate.html',theDict=dict)
+
 
 if __name__ == '__main__':
 	my_app.debug = True
